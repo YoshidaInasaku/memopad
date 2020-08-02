2408ec9a06565ab7568e8a885fa82ae668bddae1
吉田稲作 <yoshidanoinasaku@gmail.com>
2020-08-02 14:15:43 +0900

    ルーティングファイル / ログイン画面,投稿一覧画面ファイルの作成

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 'use strict';
 
-const http = require('http');
-const moment = require('moment')
+const http = require('http');    // http モジュールのインポート
+const pug = require('pug');    // pug のインポート
 
-const today = moment(new Date()).format('YYYY/MM/DD hh:mm:ss');
+const moment = require('moment')    // 時間を扱う moment.jsのインポート
 
+const today = moment(new Date()).format('YYYY/MM/DD HH:mm:ss');    // 本日の日付を取得
+
+// サーバーを設置
 const server = http.createServer( (req, res) => {
   res.writeHead(200, {
     'Content-Type': 'text/plain; charset=utf-8'
@@ -15,5 +18,6 @@ const server = http.createServer( (req, res) => {
 
 const port = 8000;
 server.listen(port, () => {
-  console.info(today + ':Listening on ' + port)
+  console.info('[' + today + ']' + 
+  ' Listening on ' + port);
 });
