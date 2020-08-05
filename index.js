cd89c4a1898a97a0fab681109aab4d7444e41990
吉田稲作 <yoshidanoinasaku@gmail.com>
2020-08-06 00:56:14 +0900

    ルーティング処理を記述するファイルとイベントハンドリングを記述するファイルを作成

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 'use strict';
-
 const http = require('http');    // http モジュールのインポート
-const pug = require('pug');    // pug のインポート
 const moment = require('moment')    // 時間を扱う moment.jsのインポート
 
 const router = require('./lib/router');    // ルーティング処理を行っているファイルをインポート
@@ -10,16 +8,13 @@ const today = moment(new Date()).format('YYYY/MM/DD HH:mm:ss');    // 本日の�
 
 // サーバーを設置
 const server = http.createServer( (req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'    // サーバーを立てた際は writeheadでステータスコードと、res として返すデータ型を規程させる
-  });
   router.route(req, res);    // ルーティング処理
 })
 .on('error', (e) => {
-  console.error('[' + today + ']' + 'Server Error: ' + e);    // error　イベントが発生したら サーバーエラーの内容をログとして表示
+  console.error('[' + today + ']' + 'Server Error: ', e);    // error　イベントが発生したら サーバーエラーの内容をログとして表示
 })
 .on('clientError', (e) => {    
-  console.error('[' + today + ']' + 'Client Error: ' + e);    // client error イベントが発生したら クライエントエラーをログとして表示
+  console.error('[' + today + ']' + 'Client Error: ', e);    // client error イベントが発生したら クライエントエラーをログとして表示
 });
 
 // サーバー接続時にログを表示させる
